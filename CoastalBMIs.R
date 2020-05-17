1efa91a72bcf38ecb6b2de5ebd54c6814c1cc48a
Ariel Levi Simons <levisimons@gmail.com>
2020-05-17 08:57:41 -0700

    Update CoastalBMIs.R

--- CoastalBMIs.R
+++ CoastalBMIs.R
@@ -47,6 +47,17 @@ communityInput <- dplyr::left_join(communityInput,assessmentInput,by=c("StationI
 
 #Read in sediment data.
 sedimentInput <- read.table("Master Data - Grainsize.csv", header=T, sep=",", as.is=T,skip=0,fill=TRUE,check.names=FALSE, encoding = "UTF-8")
+
+#Get the percent occurrence o each sediment type.
+sedimentFreq <- as.data.frame(table(sedimentInput$GrainsizeBin)/length(unique(sedimentInput$StationID)))
+colnames(sedimentFreq) <- c("GrainsizeBin","Freq")
+sedimentFreq$GrainsizeBin <- as.character(sedimentFreq$GrainsizeBin)
+
+#Select the most commonly occurring sediment types.
+sedimentCutoff <- 0.70
+sedimentList <- sedimentFreq[sedimentFreq$Freq >= sedimentCutoff, "GrainsizeBin"]
+sedimentInput <- sedimentInput[sedimentInput$GrainsizeBin %in% sedimentList,]
+
 #Reformat sediment data so the percent of each sediment type is a column.
 sedimentInput <- sedimentInput %>% group_by(GrainsizeBin) %>% mutate(n = 1:n()) %>% tidyr::spread(GrainsizeBin,Percent)
 sedimentInput$n <- NULL
@@ -140,7 +151,10 @@ sampleList <- dplyr::left_join(sampleList,tmp,by=c("Stratum","SampleYear","Condi
 sampleMax <- 15 #Minimum number of samples per stratum / year / condition score group to be considered for analysis.
 sampleList <- sampleList[sampleList$Freq >= sampleMax,]
 
+set.seed(1)
 zetaMax <- 10 #Maximum order to calculate zeta diversity.
+zetaNames <- paste("zeta",seq(1:zetaMax),sep="_") #Generate list of names for zeta diversity measures.
+zetaSDNames <- paste("zeta",seq(1:zetaMax),"sd",sep="_") #Generate list of names for the standard deviation on zeta diversity measures.
 sampleNum <- 12 #Number of samples used at a time to perform zeta diversity analysis on.
 zetaAnalysis <- data.frame()
 for(j in 1:100){
@@ -153,20 +167,14 @@ for(j in 1:100){
           data.spec <- as.data.frame(t(sampleCommunity))
           data.spec <- data.spec[sample(nrow(data.spec),sampleNum),]
           zetaDecay <- Zeta.decline.ex(data.spec,orders=1:zetaMax,rescale=TRUE,plot=FALSE)
-          zeta_1 <- Zeta.order.ex(data.spec,order=1,rescale=TRUE)$zeta.val #lower order zeta diversity measure.
-          zeta_1sd <- Zeta.order.ex(data.spec,order=1,rescale=TRUE)$zeta.val.sd #lower order zeta diversity measure standard deviation.
-          zeta_2 <- zetaDecay$zeta.val[2] #lower order zeta diversity measure.
-          zeta_2sd <- zetaDecay$zeta.val.sd[2] #lower order zeta diversity measure standard deviation.
-          zeta_3 <- zetaDecay$zeta.val[3] #lower order zeta diversity measure.
-          zeta_3sd <- zetaDecay$zeta.val.sd[3] #lower order zeta diversity measure standard deviation.
-          zeta_4 <- zetaDecay$zeta.val[4] #lower order zeta diversity measure.
-          zeta_4sd <- zetaDecay$zeta.val.sd[4] #lower order zeta diversity measure standard deviation.
-          zeta_N <- zetaDecay$zeta.val[zetaMax] #Higher order zeta diversity measure.
-          zeta_Nsd <- zetaDecay$zeta.val.sd[zetaMax] #Higher order zeta diversity measure standard deviation.
           ExpExp <- zetaDecay$zeta.exp$coefficients[2] #Zeta diversity exponential decay exponent.
           ExpAIC <- zetaDecay$aic$AIC[1] #AIC coefficient Zeta diversity exponential decay.
           PLExp <- zetaDecay$zeta.pl$coefficients[2] #Zeta diversity power law decay exponent.
           PLAIC <- zetaDecay$aic$AIC[2] #AIC coefficient Zeta diversity power law decay.
+          zetaOrders <- zetaDecay$zeta.val #Generate list of zeta diversity measures from 1 to zetaMax.
+          zetaOrders[1] <- Zeta.order.ex(data.spec,order=1,rescale=TRUE)$zeta.val #Recalculate zeta_1 as the average taxonomic richness.
+          zetaSDOrders <- zetaDecay$zeta.val.sd #Generate list of the standard deviation on zeta diversity measures from 1 to zetaMax.
+          zetaSDOrders[1] <- Zeta.order.ex(data.spec,order=1,rescale=TRUE)$zeta.val.sd #Recalculate zeta_1sd as the standard deviation on the average taxonomic richness.
           metadataSubset <- communityInput[communityInput$SampleID %in% rownames(data.spec),]
           metadataSubset <- metadataSubset[,colnames(metadataSubset) %in% c(sampleNames,chemNames,sedimentNames)]
           metadataSubset <- metadataSubset[!duplicated(metadataSubset),]
@@ -180,11 +188,11 @@ for(j in 1:100){
           sedimentData <- metadataSubset[,colnames(metadataSubset) %in% sedimentNames]
           sedimentRow <- colMeans(sedimentData,na.rm=T)
           sedimentRow[is.nan(sedimentRow)] <- NA
-          dataRow <- t(cbind(t(as.data.frame(list(c(stratumType,year,score,meanDepth,sdDepth,meanDist,sdDist,zeta_N,zeta_Nsd,zeta_1,zeta_1sd,zeta_2,zeta_2sd,zeta_3,zeta_3sd,zeta_4,zeta_4sd,ExpExp,ExpAIC,PLExp,PLAIC,as.list(as.numeric(sedimentRow)),as.list(as.numeric(chemRow))))))))
-          colnames(dataRow) <- c("stratumType","year","ConditionScore","meanDepth","sdDepth","meanDist","sdDist","zeta_N","zeta_Nsd","zeta_1","zeta_1sd","zeta_2","zeta_2sd","zeta_3","zeta_3sd","zeta_4","zeta_4sd","ExpExp","ExpAIC","PLExp","PLAIC",sedimentNames,chemNames)
+          dataRow <- t(cbind(t(as.data.frame(list(c(stratumType,year,score,meanDepth,sdDepth,meanDist,sdDist,zetaOrders,zetaSDOrders,ExpExp,ExpAIC,PLExp,PLAIC,as.list(as.numeric(sedimentRow)),as.list(as.numeric(chemRow))))))))
+          colnames(dataRow) <- c("stratumType","year","ConditionScore","meanDepth","sdDepth","meanDist","sdDist",zetaNames,zetaSDNames,"ExpExp","ExpAIC","PLExp","PLAIC",sedimentNames,chemNames)
           rownames(dataRow) <- NULL
           zetaAnalysis <- rbind(zetaAnalysis,dataRow)
-          print(paste(j,stratumType,year,score,meanDepth,sdDepth,meanDist,sdDist,zeta_N,zeta_Nsd,zeta_1,zeta_1sd,zeta_2,zeta_2sd,zeta_3,zeta_3sd,zeta_4,zeta_4sd,ExpExp,ExpAIC,PLExp,PLAIC))
+          print(paste(j,stratumType,year,score,meanDepth,sdDepth,meanDist,sdDist,zetaOrders,zetaSDOrders,ExpExp,ExpAIC,PLExp,PLAIC))
           print(chemRow)
           print(sedimentRow)
         }
@@ -193,7 +201,7 @@ for(j in 1:100){
   }
 }
 
-colnames(zetaAnalysis) <- c("stratumType","year","ConditionScore","meanDepth","sdDepth","meanDist","sdDist","zeta_N","zeta_Nsd","zeta_1","zeta_1sd","zeta_2","zeta_2sd","zeta_3","zeta_3sd","zeta_4","zeta_4sd","ExpExp","ExpAIC","PLExp","PLAIC",sedimentNames,chemNames)
+colnames(zetaAnalysis) <- c("stratumType","year","ConditionScore","meanDepth","sdDepth","meanDist","sdDist",zetaNames,zetaSDNames,"ExpExp","ExpAIC","PLExp","PLAIC",sedimentNames,chemNames)
 tmp <- zetaAnalysis$stratumType
 indx <- sapply(zetaAnalysis, is.factor)
 zetaAnalysis[indx] <- lapply(zetaAnalysis[indx], function(x) as.numeric(as.character(x)))
@@ -206,14 +214,48 @@ write.table(zetaAnalysis,paste("coastalBMIs",taxonomicLevel,".txt",sep=""),quote
 ##To run locally.
 zetaAnalysis <- read.table(paste("coastalBMIs",taxonomicLevel,".txt",sep=""), header=TRUE, sep="\t",as.is=T,skip=0,fill=TRUE,check.names=FALSE, encoding = "UTF-8")
 
-zetaModel <- lm(ConditionScore~zeta_1+zeta_2+zeta_N,data=zetaAnalysis)
+#Model the condition score as a function of zeta diversity measures.
+require(car)
+zetaModel <- lm(formula(paste("ConditionScore ~ ",paste(zetaNames,collapse="+"))),data=zetaAnalysis)
+
+calc.relimp(zetaModel)
 plot(zetaModel$model$ConditionScore,zetaModel$fitted.values)
 cor.test(zetaModel$model$ConditionScore,zetaModel$fitted.values)
 zetaAnalysis$modeledConditionScore <- zetaModel$fitted.values
 calc.relimp(zetaModel)
+#Plot model errors.
 layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
 plot(zetaModel)
 dev.off()
+
+#Check for correlation patterns between zeta diversity and environmental parameters.
+require("PerformanceAnalytics")
+environmentNames <- c(sedimentNames,chemNames,"meanDepth","meanDist")
+chart.Correlation(zetaAnalysis[,c("modeledConditionScore","ConditionScore","meanDepth","meanDist","zeta_1","zeta_2","zeta_3","zeta_4","zeta_N")], histogram=TRUE, method="pearson")
+chart.Correlation(zetaAnalysis[,c("modeledConditionScore","ConditionScore",chemNames)])
+chart.Correlation(zetaAnalysis[,c("modeledConditionScore","ConditionScore",sedimentNames)])
+
+#Model the modeled condition score as a function of environmental metrics.
+zetaModel <- lm(formula(paste("modeledConditionScore ~ ",paste(environmentNames,collapse="+"))),data=zetaAnalysis)
+calc.relimp(zetaModel)
+#Filter our variables with a high variance inflation factor.
+varList <- car::vif(zetaModel)
+varList <- names(varList[varList <= 10])
+#Model the modeled condition score as a function of the remaining environmental metrics
+zetaModel <- lm(formula(paste("modeledConditionScore ~ ",paste(varList,collapse="+"))),data=zetaAnalysis)
+calc.relimp(zetaModel)
+cor.test(zetaModel$model$modeledConditionScore,zetaModel$fitted.values)
+
+#Model the condition score as a function of environmental metrics.
+zetaModel <- lm(formula(paste("ConditionScore ~ ",paste(environmentNames,collapse="+"))),data=zetaAnalysis)
+calc.relimp(zetaModel)
+#Filter our variables with a high variance inflation factor.
+varList <- car::vif(zetaModel)
+varList <- names(varList[varList <= 10])
+#Model the condition score as a function of the remaining environmental metrics
+zetaModel <- lm(formula(paste("ConditionScore ~ ",paste(varList,collapse="+"))),data=zetaAnalysis)
+calc.relimp(zetaModel)
+cor.test(zetaModel$model$ConditionScore,zetaModel$fitted.values)
 
 # Assessing R2 shrinkage using 10-Fold Cross-Validation 
 require(bootstrap)
@@ -227,10 +269,6 @@ require(ggplot2)
 require(viridis)
 zetaPlot <- ggplot(zetaAnalysis, aes(x=ConditionScore,y=modeledConditionScore,color=meanDepth))+geom_point()+theme(text = element_text(size=25))+geom_smooth(method=glm, aes(fill=modeledConditionScore))
 zetaPlot+xlab("Condition Score")+ylab("Modeled Condition Score")+scale_color_gradientn("Mean Depth (m)",colours = rev(plasma(10)))
-
-#Check for correlation patterns between zeta diversity and environmental parameters.
-require("PerformanceAnalytics")
-chart.Correlation(zetaAnalysis[,c("modeledConditionScore","ConditionScore","meanDepth","meanDist","zeta_1","zeta_2","zeta_3","zeta_4","zeta_N")], histogram=TRUE, method="pearson")
 
 #Mapping metadata
 require(ggplot2)
